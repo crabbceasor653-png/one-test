d9b1c4604966129cda9484ef08a6bec498622404
Dashboard <crabbceasor653@gmail.com>
2026-06-03 17:26:02 +0800

    three

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/one-test/" : "/",
+  base: command === "build" ? "./" : "/",
   plugins: [react()],
 }));
